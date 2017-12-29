2ea69602fd969b2f0449495656bd82848305823d
unknown <richie.permana@gmail.com>
2017-12-29 17:20:49 +0800

    Some changes

--- app/core/App.php
+++ app/core/App.php
@@ -7,23 +7,26 @@ class App {
     protected $params     = [];
 
     public function  __construct() {
+
       $url = $this->parseUrl();
 
       if (file_exists('../app/controllers/'.$url[0].'.php')) {
         $this->controller = $url[0];
         unset($url[0]);
       }
-      
+
       require_once '../app/controllers/'. $this->controller .'.php';
       $this->controller = new $this->controller;
 
       if (isset($url[1])) {
         if (method_exists($this->controller, $url[1])) {
-          $this->controller->index();
-        } else {
-          var_dump($this->controller);
+          $this->method = $url[1];
+          unset($url[1]);
         }
       }
+
+      $this->param = $url ? array_values($url) : [];
+
     }
 
     public function parseUrl() {
