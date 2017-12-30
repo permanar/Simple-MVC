ef200d5cefd37edc98bcf37404b68427056d835f
unknown <richie.permana@gmail.com>
2017-12-30 13:05:45 +0800

    Small changes

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -6,7 +6,11 @@
 class Controller {
 
   public function model($model) {
-    require_once '../app/models/'.$model.'.php'; 
+    require_once '../app/models/'.$model.'.php';
+    return new $model();
+  }
+  public function view($view, $data=[]) {
+    require_once '../app/views/'.$view.'.php';
   }
 
 }
