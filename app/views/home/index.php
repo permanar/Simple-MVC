122803a845b5c6c18ef05a6b2b89a1c073f37b40
unknown <richie.permana@gmail.com>
2017-12-30 16:41:41 +0800

    small changes

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,28 @@
 Hello <?=$data['name']?>
+<?php
+
+        function headerModal(){
+
+          ob_start();
+          echo(" Hello there!"); //would normally get printed to the screen/output to browser
+          $output = ob_get_contents();
+          ob_get_clean();
+
+          return $output;
+        }
+
+        echo headerModal();
+
+ ?>
+
+ <?php
+ob_start();
+?>
+<div>
+    <span>text</span>
+    <a href="#">link</a>
+</div>
+<?php
+$content = ob_get_clean(); 
+echo $content;
+?>
